3ef7f0a72f47a16ef98acd138b71262ec77621e3
martabaker <marta.baker97@gmail.com>
2024-06-26 17:08:19 -0400

    finished flask routes

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -8,26 +8,54 @@ import datetime as dt
 #################################################
 # Database Setup
 #################################################
-
-
-# reflect an existing database into a new model
-
-# reflect the tables
-
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+# This is done in the sqlHelper script
+# Work is done in a separate script to separate interactive layer from queries into the database
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
+sql = SQLHelper()
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2016-08-23<br/>"
+        f"/api/v1.0/2015-04-01/2016-04-01<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    data = sql.query_precipitation()
+    return(jsonify(data))
+
+@app.route("/api/v1.0/stations")
+def stations():
+    data = sql.query_stations()
+    return(jsonify(data))
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    data = sql.query_tobs()
+    return(jsonify(data))
+
+@app.route("/api/v1.0/<start_date>")
+def tobs_dyn_start(start_date):
+    data = sql.query_tobs_dyn_start(start_date)
+    return(jsonify(data))
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def tobs_dyn_start_end(start_date, end_date):
+    data = sql.query_tobs_dyn_start_end(start_date, end_date)
+    return(jsonify(data))
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Starter_Code/sqlHelper.py
+++ Starter_Code/sqlHelper.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, text, func
 import pandas as pd
 import numpy as np
 
+
 # The Purpose of this Class is to separate out any Database logic
 class SQLHelper():
     #################################################
@@ -29,16 +30,72 @@ class SQLHelper():
     #################################################
     # Database Queries
     #################################################
-
-    def query_passengers_orm(self):
+    # Gets finds the date 1 year before the last date in the data set
+    def year_before_func(self):
         # Save reference to the table
-        Passenger = self.Base.classes.passenger
+        Measurement = self.Base.classes.measurement
 
         # Create our session (link)
         session = Session(self.engine)
 
         # Query all results
-        results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+        # Finding most recent date in the Measurement data set using ORM
+        latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+        # Making the latest date into a datetime
+        latest_date_str = pd.to_datetime(latest_date)
+
+        # Finding the year before of the date using DateOffset (gotten from: https://stackoverflow.com/a/31170136/23471668)
+        year_before = latest_date_str - pd.DateOffset(years=1)
+
+        # Convert the year_before DatetimeIndex into a date; code from Xpert
+        year_before_str = year_before.strftime('%Y-%m-%d')
+
+        # Close session
+        session.close()
+
+        return year_before_str[0]
+    
+    # Gets all precipitation data from the last year
+    def query_precipitation(self):
+        # Save reference to the table
+        Measurement = self.Base.classes.measurement
+
+        # Create our session (link)
+        session = Session(self.engine)
+
+        # Calling the year_before_func() that was defined above
+        year_before_str = self.year_before_func()
+
+        # Finding all values in the range year_before to latest_date_str
+        results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_before_str).all()
+
+        # close session
+        session.close()
+
+        df = pd.DataFrame(results)
+
+        # Creating an empty dictionary to house results
+        prec_dict = {}
+
+        # Interating over the rows in the new dataframe to fill out the dictionary with help from Xpert
+        for index, row in df.iterrows():
+            key = row['date']
+            value = row['prcp']
+            prec_dict[key] = value
+
+        return(prec_dict)
+    
+    # Returns a list of all the stations
+    def query_stations(self):
+        # Save reference to the table
+        Measurement = self.Base.classes.measurement
+
+        # Create our session (link)
+        session = Session(self.engine)
+
+        # Query the stations and then group by the stations to limit the stations listed to the 9 stations where readings were taken
+        results = session.query(Measurement.station).group_by(Measurement.station).all()
 
         # close session
         session.close()
@@ -47,25 +104,39 @@ class SQLHelper():
         data = df.to_dict(orient="records")
         return(data)
     
-    # same thing with raw SQL
-    def query_passengers_raw(self):
-
-        # Query all results
-        query = "Select name, age, sex from passenger;"
-
-        df = pd.read_sql(text(query), con = self.engine)
-        data = df.to_dict(orient="records")
-        return(data)
-    
-    def query_dynamic_orm(self, min_age, gender):
+    # Gets the station id for the most active station
+    def most_active_station(self):
         # Save reference to the table
-        Passenger = self.Base.classes.passenger
+        Measurement = self.Base.classes.measurement
 
         # Create our session (link)
         session = Session(self.engine)
 
-        # Query all results
-        results = session.query(Passenger.name, Passenger.age, Passenger.sex).filter(Passenger.age >= min_age).filter(Passenger.sex == gender).order_by(Passenger.name).all()
+        # Find the most active station
+        station_act = session.query(Measurement.station, func.count(Measurement.station)).\
+            group_by(Measurement.station).\
+            order_by(func.count(Measurement.station).desc()).\
+            all()
+        
+        # station_act[0][0] houses the namne of the most active station
+        return station_act[0][0]
+    
+    # Gets dates and temperature observations for the most-active station for the previous year
+    def query_tobs(self):
+        # Save reference to the table
+        Measurement = self.Base.classes.measurement
+
+        # Create our session (link)
+        session = Session(self.engine)
+
+        # Find the most active station
+        ma_station = self.most_active_station()
+
+        # Calling the year_before_func() that was defined above
+        year_before_str = self.year_before_func()
+
+        # Last year precipitation information with both date and temperature
+        results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_before_str, Measurement.station == ma_station).all()
 
         # close session
         session.close()
@@ -74,22 +145,51 @@ class SQLHelper():
         data = df.to_dict(orient="records")
         return(data)
     
-    def query_dynamic_raw(self, min_age, gender):
+    
+    def query_tobs_dyn_start(self, start_date):
+        # Save reference to the table
+        Measurement = self.Base.classes.measurement
 
-        # Query all results
-        query = f"""
-                Select
-                    name,
-                    age,
-                    sex
-                From
-                    passenger
-                Where
-                    age >= {min_age}
-                    AND sex = '{gender}'
-                Order by
-                    name asc
-                """
-        df = pd.read_sql(text(query), con = self.engine)
+        # Create our session (link)
+        session = Session(self.engine)
+
+        # Find the most active station
+        ma_station = self.most_active_station()
+
+        # Find the temp stats for dates after the start date
+        results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.station == ma_station, Measurement.date >= f'{start_date}').all()
+
+        # close session
+        session.close()
+
+        # Set column names:
+        cols = [f"min temp from {start_date}", f"max temp from {start_date}", f"avg temp from {start_date}"]
+
+        df = pd.DataFrame(results, columns=cols)
+        data = df.to_dict(orient="records")
+        return(data)
+    
+    def query_tobs_dyn_start_end(self, start_date, end_date):
+       # Save reference to the table
+        Measurement = self.Base.classes.measurement
+
+        # Create our session (link)
+        session = Session(self.engine)
+
+         # Find the most active station
+        ma_station = self.most_active_station()
+
+        # Find the temp stats for dates between (and including) the start and end dates
+        results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+        filter(Measurement.station == ma_station, Measurement.date >= f'{start_date}', Measurement.date <= f'{end_date}').all()
+
+        # close session
+        session.close()
+
+        # Set column names:
+        cols = [f"min temp beween {start_date} and {end_date}", f"max temp beween {start_date} and {end_date}", f"avg temp beween {start_date} and {end_date}"]
+
+        df = pd.DataFrame(results, columns=cols)
         data = df.to_dict(orient="records")
         return(data)
